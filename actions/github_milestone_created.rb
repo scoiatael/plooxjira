3dc85508d282f1ba4778e3ce405c4abe6b9b4876
scoiatael <czapl.luk+git@gmail.com>
2017-03-03 15:01:28 +0100

    Forgot to call actions

--- actions/github_milestone_created.rb
+++ actions/github_milestone_created.rb
@@ -7,10 +7,10 @@ class GithubMilestoneCreated
   end
 
   def call(params)
-    issue_key = CreateJiraStory.new(title: @title, url: @url)
+    issue_key = CreateJiraStory.new(title: @title, url: @url).call(params)
     puts "Created Jira issue with key #{issue_key}"
     SetMilestoneTitle.new(repository: params['repository']['full_name'],
                           number: @number,
-                          title: "[#{issue_key}] #{@title}")
+                          title: "[#{issue_key}] #{@title}").call(params)
   end
 end
