add913f548fcedc5a92c3770291e4875fcd6ce39
scoiatael <czapl.luk+git@gmail.com>
2017-03-03 11:05:09 +0100

    Don't try to serialize Hash

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# Api entrypoint
 class App < Sinatra::Base
   before do
     request.body.rewind
@@ -7,9 +8,8 @@ class App < Sinatra::Base
   post '/payload' do
     signature = env['X-Hub-Signature'] || env['HTTP_X_HUB_SIGNATURE']
     puts "Signature: #{signature}"
-    body_hmac = CalculateHubSignature.new.call(params)
-    body_hmac2 = CalculateHubSignature.new.call(@body)
-    puts "Calculated: #{body_hmac}, #{body_hmac2}"
+    body_hmac = CalculateHubSignature.new.call(@body)
+    puts "Calculated: #{body_hmac}"
     'OK'
   end
 end
