b4ad8592b17b4e916330503a0a7bebea3e9b93f8
scoiatael <czapl.luk+git@gmail.com>
2017-04-27 10:04:59 +0200

    WTF ruby syntax

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-Dir['config/*.rb', 'actions/github/*.rb', 'actions/jira/*.rb' 'actions/*.rb'].sort.each do |f|
+Dir['config/*.rb', 'actions/github/*.rb', 'actions/jira/*.rb', 'actions/*.rb'].sort.each do |f|
   require_relative f
 end
 require_relative 'app'
