619fa5a332f85bbce965ab6cde98dc8e5a6bdc98
scoiatael <czapl.luk+git@gmail.com>
2017-05-11 09:45:24 +0200

    Guard against adding Jira story to milestone with one

--- actions/github_milestone_created.rb
+++ actions/github_milestone_created.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Handler for creation of Github milestone
 class GithubMilestoneCreated
   def initialize(milestone)
@@ -7,6 +8,9 @@ class GithubMilestoneCreated
   end
 
   def call(params)
+    existing_issue = FindGithubAction.extract_jira_key('title' => @title)
+    return if existing_issue
+
     issue_key = CreateJiraStory.new(title: @title, url: @url).call(params)
     puts "Created Jira issue with key #{issue_key}"
     SetMilestoneTitle.new(repository: params['repository']['full_name'],
