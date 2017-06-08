5c86425c049e10496315843132a5e68f32d0f65c
scoiatael <czapl.luk+git@gmail.com>
2017-06-08 16:13:30 +0200

    fixup! Set milestone description when new milestone is opened

--- actions/github_milestone_created.rb
+++ actions/github_milestone_created.rb
@@ -5,13 +5,16 @@ class GithubMilestoneCreated
     @title = milestone['title']
     @url = milestone['url']
     @number = milestone['number']
+    @description = milestone['description']
   end
 
   def call(params)
     existing_issue = FindGithubAction.extract_jira_key('title' => @title)
     return if existing_issue
 
-    issue_key = CreateJiraStory.new(title: @title, url: @url).call(params)
+    issue_key = CreateJiraStory.new(title: @title,
+                                    url: @url,
+                                    description: @description).call(params)
     puts "Created Jira issue with key #{issue_key}"
     SetMilestoneTitle.new(repository: params['repository']['full_name'],
                           number: @number,
